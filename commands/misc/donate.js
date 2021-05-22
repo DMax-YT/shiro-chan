2f4a821b4a393e5f4685442ffa7da4c9d35760bd
DMax-YT <maxim.borisov321@gmail.com>
2021-05-22 17:08:01 +0300

    Donate command

---
+++ commands/misc/donate.js
@@ -0,0 +1,23 @@
+const {
+  Util: { resolveColor },
+} = require("discord.js");
+const translate = require("../../helpers/locale");
+const { embedInvis } = require("../../colors.json");
+
+async function donate(msg, args, locale) {
+  msg.channel.send({
+    embed: {
+      description: `**[Patreon](https://www.patreon.com/dmax_programmer)**\n**[Boosty](https://boosty.to/dmax)**`,
+      color: resolveColor(embedInvis),
+    },
+  });
+}
+
+module.exports = {
+  name: "donate",
+  execute: donate,
+  alias: [],
+  argsRequired: 0,
+  module: "Misc",
+  isPrivate: false,
+};
