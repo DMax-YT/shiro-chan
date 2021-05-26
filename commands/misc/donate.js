7473814e200770e3798ca7c23eb23c4e63b5c761
DMax-YT <maxim.borisov321@gmail.com>
2021-05-26 22:26:14 +0300

    Donate command update with buttons

--- commands/misc/donate.js
+++ commands/misc/donate.js
@@ -1,15 +1,28 @@
-const {
-  Util: { resolveColor },
-} = require("discord.js");
 const translate = require("../../helpers/locale");
-const { embedInvis } = require("../../colors.json");
 
 async function donate(msg, args, locale) {
-  msg.channel.send({
-    embed: {
-      title: translate("donate.support", locale),
-      description: `**[Patreon](https://www.patreon.com/dmax_programmer)**\n**[Boosty](https://boosty.to/dmax)**`,
-      color: resolveColor(embedInvis),
+  msg.client.api.channels[msg.channel.id].messages.post({
+    data: {
+      content: `**${translate("donate.support", locale)}**`,
+      components: [
+        {
+          type: 1,
+          components: [
+            {
+              type: 2,
+              label: "Patreon",
+              style: 5,
+              url: "https://www.patreon.com/dmax_programmer",
+            },
+            {
+              type: 2,
+              label: "Boosty",
+              style: 5,
+              url: "https://boosty.to/dmax",
+            },
+          ],
+        },
+      ],
     },
   });
 }
