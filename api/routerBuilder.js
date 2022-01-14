c02e564b0b9e12cc7b205c967b1c1545a11efe6d
DMax-YT <maxim.borisov321@gmail.com>
2022-01-14 20:45:40 +0300

    Improve router builder

--- api/routerBuilder.js
+++ api/routerBuilder.js
@@ -1,16 +1,18 @@
 /**
+ * @template T
  * @param {import("axios").AxiosInstance} apiBase
- * @param {Object<string, string>} routes
+ * @param {T} routes
  * @param {string} suffix
+ * @returns {{[K in keyof T]: () => Promise<string>}}
  */
-const buildRouter = (apiBase, routes, suffix = "/") =>
-  new Proxy(routes, {
-    get: (routes, property) =>
-      routes.hasOwnProperty(property)
-        ? new Proxy(apiBase.get.bind(apiBase, `${routes[property]}${suffix}`), {
-            apply: (request) => request().then((res) => res.data),
-          })
-        : () => new Promise((_, reject) => reject("No such route")),
-  });
+const buildRouter = (apiBase, routes, suffix = "/") => {
+  const newRoutes = {};
+  for (const key in routes) {
+    newRoutes[key] = () =>
+      apiBase.get(apiBase, `${routes[key]}${suffix}`).then((res) => res.data);
+  }
+
+  return newRoutes;
+};
 
 module.exports = buildRouter;
